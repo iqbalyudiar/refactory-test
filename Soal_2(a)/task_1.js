e52ec42949fe78d9552e58ff25842ca69174304a
iqbal <iqbal@grip-principle.com>
2021-01-28 14:38:46 +0700

    remove unused data

--- Soal_2(a)/task_1.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-let rawdata = fs.readFileSync("profile_list.json");
-let users = JSON.parse(rawdata);
-
-let userName = users.filter((user) => user.profile.phones.length < 1);
-
-console.log("Answer of Exercise Number 1 \n", userName);
--- Soal_2(a)/task_2.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-let rawdata = fs.readFileSync("profile_list.json");
-let users = JSON.parse(rawdata);
-
-// console.log(users);
-
-const articles = users.filter((user) => user.articles.length > 0);
-console.log("Answer of Exercise Number 2 \n", articles);
